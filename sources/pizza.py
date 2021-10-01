c2b6a7940238c11561561c09d86629fa9d942a09
Denis <deph92@gmail.com>
2021-10-01 13:49:52 +0200

    Nouveau menu

--- sources/pizza.py
+++ sources/pizza.py
@@ -35,5 +35,9 @@ RECIPES = {
     'mediterranea': {
         'ingredients': {'mozzarella': 3, 'pepper': 4, 'onion': 3},
         'price': 11,
+    },
+    'paysanne': {
+        'ingredients': {'ham': 3, 'champignon': 2, 'tomate': 3, 'onion': 2},
+        'price': 11,
     }
 }
